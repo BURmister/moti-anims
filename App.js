af3de66b31170bf9f54ebf2a9697d530c9e8f131
BURmister <olegcernysov903@gmail.com>
2024-03-14 23:37:54 +0300

    fix versions conflicts

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
+import 'react-native-reanimated';
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, useColorScheme, StyleSheet } from 'react-native';
+import { useRef } from 'react';
+
+import { MotiView } from 'moti';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+   const _scheme = useColorScheme();
+   const colors = useRef({
+      background: _scheme === 'light' ? '#fff' : '#000',
+      font: _scheme === 'light' ? '#000' : '#fff',
+      ui: _scheme === 'light' ? '#000' : '#fff',
+   });
+
+   return (
+      <View style={[styles.container, { backgroundColor: colors.current.background }]}>
+         <MotiView
+            from={{
+               width: 100,
+               height: 100,
+               borderRadius: 100 / 2,
+               borderWidth: 0,
+               opacity: 0,
+            }}
+            animate={{
+               width: 100 + 20,
+               height: 100 + 20,
+               borderRadius: (100 + 20) / 2,
+               borderWidth: 100 / 10,
+               opacity: 1,
+            }}
+            transition={{
+               type: 'timing',
+               duration: 1000,
+               loop: true,
+            }}
+            style={{
+               width: 100,
+               height: 100,
+               borderRadius: 100 / 2,
+               borderWidth: 100 / 10,
+               borderColor: colors.current.ui,
+               shadowColor: colors.current.ui,
+               shadowOffset: { width: 0, height: 0 },
+               shadowOpacity: 1,
+               shadowRadius: 10,
+            }}></MotiView>
+         <StatusBar style="auto" />
+      </View>
+   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+   container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+   },
 });
